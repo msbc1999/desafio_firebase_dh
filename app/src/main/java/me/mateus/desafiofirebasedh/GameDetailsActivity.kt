a1d2649a25906d08501dd81d04469e2533e86cd0
Mateus Câmara <mc.email.1999@gmail.com>
2021-01-23 22:26:10 -0300

    Criação dos Layouts: Splash, Login, Register, Main, GameDetails e GameRegister

---
+++ app/src/main/java/me/mateus/desafiofirebasedh/GameDetailsActivity.kt
@@ -0,0 +1,11 @@
+package me.mateus.desafiofirebasedh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_details)
+    }
+}
